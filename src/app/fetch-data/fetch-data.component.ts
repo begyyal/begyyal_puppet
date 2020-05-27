48fd95e18f056a7efd703803035c3ebd423df9ba
ra-men-man <ololo@ololo.ll>
2020-05-28 04:01:51 +0900

    e2e整備

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-fetch-data',
@@ -9,7 +10,7 @@ export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+    http.get<WeatherForecast[]>(baseUrl + 'app/weatherforecast').subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
